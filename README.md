6755b784056ae18b06e18b1b8ccaf5b955cf9c1e
m-underwood <56260924+m-underwood@users.noreply.github.com>
2020-04-29 11:18:12 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PPM-new
+# NTU - PPM
+Practical Project Management
+
+# Group Members
+-Abdul Basit <br>
+-Balazs Szilagyi <br>
+-Cai Lin <br>
+-Jay Chim <br>
+-Kelvin Lau <br>
+-Matthew Underwood <br> 
+
---
+++ connectdatabase.php
@@ -0,0 +1,19 @@
+<?php
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "ppm_database";
+
+    $con = mysqli_connect($host, $username, $password, $db);
+    
+    if(mysqli_connect_errno())
+    {
+        echo "Failed to connect to Sql server".mysqli_connect_error();
+    }
+    else
+    {
+        echo "success";
+    }
+
+?>
