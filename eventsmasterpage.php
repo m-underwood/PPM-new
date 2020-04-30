83f2e1b73eb9a19e77d334f1ca7ba5cf0b0a195b
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 19:20:01 +0100

    Update eventsmasterpage.php

--- eventsmasterpage.php
+++ eventsmasterpage.php
@@ -32,7 +32,7 @@
     <body>
 
 
-        <?php
+          <?php
             echo "<br><br><a href=inserteventsform.php>Insert</a>";
 
             while($row = mysqli_fetch_assoc($result))
@@ -45,9 +45,10 @@
                 "<br> Starting Date: " . $row["begin_date"].
                 "<br> Ending Date: " . $row["end_date"].
                 "<br> Starting Time: " . $row["start_time"].
-                "<br> Community ID: " . $row["community_id"].
                 "<br> Details: " . $row["details"].
                 "<br> Comments: " . $row["comments"].
+                "<br> Community Information: " . $row["community_info"].
+                "<br> Community Contact: " . $row["community_contact"].
                 "<br><a href=events.php?ID=". $row["id"].">See further details</a>".
                "<br><a href=deleteevent.php?ID=". $row["id"].">Delete Entry</a>".
                 "<br><a href=updateform.php?ID=". $row["id"].">Update Event</a>".
