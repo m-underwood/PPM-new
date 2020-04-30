48a092b887d91154ae9e94f768ee6d45865adeb7
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 16:39:09 +0100

    Added more columns

--- insertevents.php
+++ insertevents.php
@@ -3,20 +3,29 @@
     include "connectdatabase.php";
 
     //check fields are not empty
-    if(isset($_POST["id"])){
+    if(isset($_POST["id"])){           
         $id = $_POST["id"];
+    }
+    if(isset($_POST["event_name"])){    
+        $event_name = $_POST["event_name"];
     }
     if(isset($_POST["location_name"])){
         $location_name = $_POST["location_name"];
     }
-    if(isset($_POST["starting_date"])){
-        $starting_date = $_POST["starting_date"];
+    if(isset($_POST["officers"])){
+        $officers = $_POST["officers"];
     }
-    if(isset($_POST["ending_date"])){
-        $ending_date = $_POST["ending_date"];
+    
+    if(isset($_POST["begin_date"])){
+        $begin_date = $_POST["begin_date"];
     }
-    if(isset($_POST["starting_time"])){
-        $starting_time = $_POST["starting_time"];
+
+    if(isset($_POST["end_date"])){
+        $end_date = $_POST["end_date"];
+    }
+
+    if(isset($_POST["start_time"])){
+        $start_time = $_POST["start_time"];
     }
     if(isset($_POST["details"])){
         $details = $_POST["details"];
@@ -26,7 +35,7 @@
     }
 
     //insert values into the database
-    $sql = "INSERT INTO events_tbl ( id, location_name, starting_date, ending_date, starting_time, details, comments) VALUES ('$id','$location_name','$starting_date', '$ending_date', '$starting_time', '$details', '$comments')";
+    $sql = "INSERT INTO events_tbl ( id, event_name, location_name, officers, begin_date, end_date, start_time, details, comments) VALUES ('$id', '$event_name', '$location_name', '$officers', '$begin_date', '$end_date', '$start_time', '$details', '$comments')";
 
     //if values are successful redirect back to events.php or print error message
 
