54af8bd51f8145d0e32c4141a849e1098dde76b1
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 16:39:38 +0100

    Updated Form

--- inserteventsform.php
+++ inserteventsform.php
@@ -10,10 +10,12 @@
 <body>
         <form action="insertevents.php" method = "post">
         ID: <input type ="text" name = "id">
+        <p>Event Name: <input type = "text" name = "event_name"></p>
         <p>Location: <input type = "text" name = "location_name"></p>
-        <p>Starting Date: <input type = "date" name = "starting_date"></p>
-        <p>Ending Date: <input type = "date" name = "ending_date"></p>
-        <p>Starting Time: <input type = "text" name ="starting_time"></p>
+        <p>Officers: <input type = "text" name ="officers"></p>
+        <p>Starting Date: <input type = "date" name = "begin_date"></p>
+        <p>Ending Date: <input type = "date" name = "end_date"></p>
+        <p>Starting Time: <input type = "text" name ="start_time"></p>
         <p>Details: <input type = "text" name ="details"></p>
         <p>Comments: <input type = "text" name= "comments"></p>
         
