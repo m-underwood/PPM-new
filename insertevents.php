0148de018910c3aeaf11ffc2d2a3f7ebecbebc6b
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 19:19:30 +0100

    Update insertevents.php

--- insertevents.php
+++ insertevents.php
@@ -28,10 +28,6 @@
         $start_time = $_POST["start_time"];
     }
 
-    if(isset($_POST["community_id"])){
-        $community_id = $_POST["community_id"];
-    }
-
     if(isset($_POST["details"])){
         $details = $_POST["details"];
     }
@@ -39,8 +35,16 @@
         $comments = $_POST["comments"];
     }
 
+    if(isset($_POST["community_info"])){
+        $community_info = $_POST["community_info"];
+    }
+
+    if(isset($_POST["community_contact"])){
+        $community_contact = $_POST["community_contact"];
+    }
+
     //insert values into the database
-    $sql = "INSERT INTO events_tbl ( id, event_name, location_name, officers, begin_date, end_date, start_time, community_id, details, comments) VALUES ('$id', '$event_name', '$location_name', '$officers', '$begin_date', '$end_date', '$start_time', '$community_id', '$details', '$comments')";
+    $sql = "INSERT INTO events_tbl ( id, event_name, location_name, officers, begin_date, end_date, start_time, details, comments, community_info, community_contact) VALUES ('$id', '$event_name', '$location_name', '$officers', '$begin_date', '$end_date', '$start_time', '$details', '$comments', '$community_info', '$community_contact')";
 
     //if values are successful redirect back to events.php or print error message
 
