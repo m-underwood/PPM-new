f7708eb6c2686f8365b3dee65886b47cda15ee6d
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 11:38:34 +0100

    Update updateform.php

--- updateform.php
+++ updateform.php
@@ -2,7 +2,7 @@
 
     include "connectdatabase.php";
     $id = $_REQUEST["ID"];
-    $sql = "SELECT * FROM events_tbl WHERE ID = 'id'";
+    $sql = "SELECT * FROM events_tbl WHERE ID = '$id'";
 
     $result = mysqli_query($con, $sql);
     $row = mysqli_fetch_assoc($result);
@@ -22,7 +22,7 @@
     Location: <input type = "text" name = "location_name" value = <?php echo $row["location_name"];?>><p>
     Starting Date: <input type = "date" name = "starting_date" value = <?php echo $row["starting_date"];?>><p>
     Ending Date: <input type = "date" name = "ending_date" value = <?php echo $row ["ending_date"];?>><p>
-    Starting Time: <input type = "text" name = "starting_date" value = <?php echo $row["starting_time"];?>><p>
+    Starting Time: <input type = "text" name = "starting_time" value = <?php echo $row["starting_time"];?>><p>
     Details: <input type = "text" name = "details" value = <?php echo $row["details"];?>><p>
     Comments: <input type = "text" name = "comments" value = <?php echo $row["comments"];?>><p>
     <input type = "submit">
