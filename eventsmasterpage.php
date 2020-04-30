36fe9ec9faca23c26b4d1cf5bc6bc67f25bbcfe7
m-underwood <56260924+m-underwood@users.noreply.github.com>
2020-04-30 15:59:45 +0100

    Update eventsmasterpage.php

--- eventsmasterpage.php
+++ eventsmasterpage.php
@@ -8,7 +8,7 @@
     @$combineddate = $year.-$month.-$day;
 
     if ((!is_null($day)) and (!is_null($month)) and (!is_null($year))){
-        echo "<br><br>Nothing is null.<br><br>";
+        echo "Nothing is null.<br>";
         echo "Day: " . $day . ", Month: " . $month . ", Year: " . $year . ", Combined: " . $combineddate;
 
 
@@ -32,6 +32,8 @@
     <body>
         <?php
         
+            echo "<br><br><a href=inserteventsform.php>Insert</a>";
+
             while($row = mysqli_fetch_assoc($result))
             {
                 echo "<div>".
@@ -43,7 +45,6 @@
                 "<br> Details: " . $row["details"].
                 "<br> Comments: " . $row["comments"].
                 "<br><a href=events.php?ID=". $row["id"].">See further details</a>".
-                "<br><a href=inserteventsform.php?ID=". $row["id"].">Insert</a>".
                 "<br><a href=deleteevent.php?ID=". $row["id"].">Delete Entry</a>".
                 "<br><a href=updateform.php?ID=". $row["id"].">Update Event</a>".
                 "</p>".
