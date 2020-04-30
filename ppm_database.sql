43cfae9d845cdc56cdb909830de288a2237c781f
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 17:09:04 +0100

    Update ppm_database.sql

--- ppm_database.sql
+++ ppm_database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2020 at 04:20 PM
+-- Generation Time: Apr 30, 2020 at 06:08 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.30
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `communities_tbl`
+--
+
+CREATE TABLE `communities_tbl` (
+  `id` int(11) NOT NULL,
+  `communication_channel` varchar(255) DEFAULT NULL,
+  `contact_phone` varchar(255) DEFAULT NULL,
+  `contact_email` varchar(255) DEFAULT NULL,
+  `contact_name` varchar(255) DEFAULT NULL,
+  `location_name` varchar(255) DEFAULT NULL,
+  `current_engagement` varchar(255) DEFAULT NULL,
+  `details` varchar(255) DEFAULT NULL,
+  `comments` varchar(255) DEFAULT NULL,
+  `community_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `communities_tbl`
+--
+
+INSERT INTO `communities_tbl` (`id`, `communication_channel`, `contact_phone`, `contact_email`, `contact_name`, `location_name`, `current_engagement`, `details`, `comments`, `community_name`) VALUES
+(1, 'Phone Number Preferred', '07459320560', 'Test@test.co.uk', 'Matthew', 'NTU', 'once a weeek', 'greggs has closed', 'he needs immediate medical attention', NULL),
+(2, 'Email address', '02650560503', 'Test_test@testing.co.uk', 'Database', 'NTU', '7 time a week', 'Please send help', 'i have no idea what i am doing', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `events_tbl`
 --
 
@@ -32,32 +59,48 @@ CREATE TABLE `events_tbl` (
   `event_name` varchar(255) DEFAULT NULL,
   `location_name` varchar(255) DEFAULT NULL,
   `officers` varchar(255) DEFAULT NULL,
-  `starting_date` date DEFAULT NULL,
-  `ending_date` date DEFAULT NULL,
-  `starting_time` time DEFAULT NULL,
+  `begin_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
   `details` varchar(255) DEFAULT NULL,
-  `comments` varchar(255) DEFAULT NULL
+  `comments` varchar(255) DEFAULT NULL,
+  `community_id` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `events_tbl`
 --
 
-INSERT INTO `events_tbl` (`id`, `event_name`, `location_name`, `officers`, `starting_date`, `ending_date`, `starting_time`, `details`, `comments`) VALUES
-(0, NULL, 'test', NULL, '2020-04-23', '2020-04-30', '12:00:00', 'placeholder', ''),
-(2, NULL, 'National', NULL, '2020-09-10', '2020-09-10', '00:00:00', 'Suicide prevention day', NULL),
-(10, NULL, 'Police Station', NULL, '2020-04-09', '2020-04-30', '12:12:12', 'added to database', '1st attempt'),
-(15, NULL, 'Police Station', NULL, '2020-04-07', '2020-04-30', '12:12:12', 'added to database', '1st attempt');
+INSERT INTO `events_tbl` (`id`, `event_name`, `location_name`, `officers`, `begin_date`, `end_date`, `start_time`, `details`, `comments`, `community_id`) VALUES
+(2, 'Holiday', 'Police Station', 'Matthew, Billy', '2020-04-20', '0000-00-00', '05:00:00', 'added to database', 'Your screwed', NULL),
+(60, 'Holiday', 'Police Station', 'Matthew, Billy', '2020-04-08', '2020-04-23', '05:00:00', 'added to database', 'it was the $sign', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `communities_tbl`
+--
+ALTER TABLE `communities_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `events_tbl`
 --
 ALTER TABLE `events_tbl`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `community_id` (`community_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events_tbl`
+--
+ALTER TABLE `events_tbl`
+  ADD CONSTRAINT `events_tbl_ibfk_1` FOREIGN KEY (`community_id`) REFERENCES `communities_tbl` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
