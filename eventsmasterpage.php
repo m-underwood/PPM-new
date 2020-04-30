18073e2f92c8fea2ac81abc19e3b05085ff31a09
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 17:05:56 +0100

    Update eventsmasterpage.php

--- eventsmasterpage.php
+++ eventsmasterpage.php
@@ -8,11 +8,11 @@
     @$combineddate = $year.-$month.-$day;
 
     if ((!is_null($day)) and (!is_null($month)) and (!is_null($year))){
-        echo "Nothing is null.<br>";
+        echo "<br><br>Nothing is null.<br><br>";
         echo "Day: " . $day . ", Month: " . $month . ", Year: " . $year . ", Combined: " . $combineddate;
 
 
-        $sql = "SELECT * FROM events_tbl WHERE '$combineddate' BETWEEN starting_date AND ending_date";
+        $sql = "SELECT * FROM events_tbl WHERE '$combineddate' BETWEEN begin_date AND end_date";
     }
     else {
         $sql = "SELECT * FROM events_tbl";
@@ -30,22 +30,26 @@
         <!--link rel="stylesheet"  href="css/stylesheet.css"-->
     </head>
     <body>
+
+
         <?php
-        
             echo "<br><br><a href=inserteventsform.php>Insert</a>";
 
             while($row = mysqli_fetch_assoc($result))
             {
                 echo "<div>".
                 "<p> ID: " . $row["id"].
+                "<br> Event Name: ". $row["event_name"].
                 "<br> Location: ". $row["location_name"].
-                "<br> Starting Date: " . $row["starting_date"].
-                "<br> Ending Date: " . $row["ending_date"].
-                "<br> Starting Time: " . $row["starting_time"].
+                "<br> Officers: ". $row["officers"].
+                "<br> Starting Date: " . $row["begin_date"].
+                "<br> Ending Date: " . $row["end_date"].
+                "<br> Starting Time: " . $row["start_time"].
+                "<br> Community ID: " . $row["community_id"].
                 "<br> Details: " . $row["details"].
                 "<br> Comments: " . $row["comments"].
                 "<br><a href=events.php?ID=". $row["id"].">See further details</a>".
-                "<br><a href=deleteevent.php?ID=". $row["id"].">Delete Entry</a>".
+               "<br><a href=deleteevent.php?ID=". $row["id"].">Delete Entry</a>".
                 "<br><a href=updateform.php?ID=". $row["id"].">Update Event</a>".
                 "</p>".
                 "</div>";
