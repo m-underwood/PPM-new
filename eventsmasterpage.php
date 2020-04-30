62dfd02249614f4649b9ea11a0f8d9a9b2afcbb6
m-underwood <56260924+m-underwood@users.noreply.github.com>
2020-04-30 15:15:49 +0100

    Update eventsmasterpage.php
    Added handling for no dynamic URL.

--- eventsmasterpage.php
+++ eventsmasterpage.php
@@ -2,18 +2,23 @@
 <?php
     include "connectdatabase.php";
 
-    $day = $_REQUEST['day'];
-    $month = $_REQUEST['month'];
-    $year = $_REQUEST['year'];
+    @$day = $_REQUEST['day'];
+    @$month = $_REQUEST['month'];
+    @$year = $_REQUEST['year'];
+    @$combineddate = $year.-$month.-$day;
 
-    /*if (!is_null($day)) and (!is_null($month)) and (!is_null($month)){
-        $combineddate = $year.-$month.-$day;
+    if ((!is_null($day)) and (!is_null($month)) and (!is_null($year))){
+        echo "<br><br>Nothing is null.<br><br>";
         echo "Day: " . $day . ", Month: " . $month . ", Year: " . $year . ", Combined: " . $combineddate;
-    }*/
+
+
+        $sql = "SELECT * FROM events_tbl WHERE '$combineddate' BETWEEN starting_date AND ending_date";
+    }
+    else {
+        $sql = "SELECT * FROM events_tbl";
+    }
 
     //if theres a dynamic url { $sql = "SELECT * FROM events_tbl WHERE '$combineddate' BETWEEN starting_date AND ending_date";} else { $sql = "SELECT * FROM events_tbl WHERE starting_date = '$combineddate'"; }
-    
-    $sql = "SELECT * FROM events_tbl WHERE '$combineddate' BETWEEN starting_date AND ending_date";
     
     $result = mysqli_query($con, $sql);
     
