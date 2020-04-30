75abd10092ba12b286d11273f87b0831f72ede26
NormallyStrange1 <56260922+NormallyStrange1@users.noreply.github.com>
2020-04-30 19:47:52 +0100

    Update update.php

--- update.php
+++ update.php
@@ -3,21 +3,32 @@
     include "connectdatabase.php";
 
     //check fields are not empty
-    if(isset($_POST["id"])){
+    if(isset($_POST["id"])){           
         $id = $_POST["id"];
+    }
+    if(isset($_POST["event_name"])){    
+        $event_name = $_POST["event_name"];
     }
     if(isset($_POST["location_name"])){
         $location_name = $_POST["location_name"];
     }
-    if(isset($_POST["starting_date"])){
-        $starting_date = $_POST["starting_date"];
+    if(isset($_POST["officers"])){
+        $officers = $_POST["officers"];
     }
-    if(isset($_POST["ending_date"])){
-        $ending_date = $_POST["ending_date"];
+    
+    if(isset($_POST["begin_date"])){
+        $begin_date = $_POST["begin_date"];
     }
-    if(isset($_POST["starting_time"])){
-        $starting_time = $_POST["starting_time"];
+
+    if(isset($_POST["end_date"])){
+        $end_date = $_POST["end_date"];
     }
+
+    if(isset($_POST["start_time"])){
+        $start_time = $_POST["start_time"];
+        
+    }
+    
     if(isset($_POST["details"])){
         $details = $_POST["details"];
     }
@@ -25,7 +36,26 @@
         $comments = $_POST["comments"];
     }
 
-    $sql = "UPDATE events_tbl SET location_name = '$location_name', starting_date = '$starting_date', ending_date = '$ending_date', starting_time = '$starting_time',details = '$details', comments = '$comments' WHERE ID = '$id'";
+    if(isset($_POST["community_info"])){
+        $community_info = $_POST["community_info"];
+    }
+
+    if(isset($_POST["community_contact"])){
+        $community_contact = $_POST["community_contact"];
+    }
+
+    $sql = "UPDATE events_tbl SET 
+    event_name = '$event_name', 
+    location_name = '$location_name',
+    officers = '$officers', 
+    begin_date = '$begin_date', 
+    end_date = '$end_date', 
+    start_time = '$start_time', 
+    details = '$details', 
+    comments = '$comments',
+    community_info = '$community_info',
+    community_contact = '$community_contact'
+    WHERE ID = '$id'";
 
     if(mysqli_query($con, $sql)){
         header("Location: eventsmasterpage.php");
