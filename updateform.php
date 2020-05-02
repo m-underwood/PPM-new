db046d0ed5ca4b85b15fe670cb2248e804390631
m-underwood <56260924+m-underwood@users.noreply.github.com>
2020-05-02 22:48:30 +0100

    Update updateform.php

--- updateform.php
+++ updateform.php
@@ -12,33 +12,32 @@
 <DOCTYPE html>
 <html>
 
-<head>
-
-</head>
-
-<body>
-
-    <form action = "update.php" method = "post">
-
+    <head>
+    <link rel="stylesheet" href="css/databaseCss.css">
     
+    </head>
 
-    
-    ID: <input type = "text" name = "id" value = <?php echo $row["id"];?> readonly>
-    <p>Event Name: <input type = "text" name = "event_name" value = <?php echo $row["event_name"];?>>
-    <p>Location: <input type = "text" name = "location_name" value = <?php echo $row["location_name"];?>>
-    <p>Officers: <input type = "text" name ="officers" value = <?php echo $row["officers"];?>>
-    <p>Starting Date: <input type = "date" name = "begin_date" value = <?php echo $row["begin_date"];?>>
-    <p>Ending Date: <input type = "date" name = "end_date" value = <?php echo $row ["end_date"];?>>
-    <p>Starting Time: <input type = "text" name = "start_time" value = <?php echo $row["start_time"];?>>
-    <p>Details: <input type = "text" name = "details" value = <?php echo $row["details"];?>>
-    <p>Comments: <input type = "text" name = "comments" value = <?php echo $row["comments"];?>>
-    <p>Community Information: <input type = "text" name = "community_info" value = <?php echo $row["community_info"];?>>
-    <p>Community Contact: <input type = "text" name = "community_contact" value = <?php echo $row["community_contact"];?>>
-    
-
-    <input type = "submit">
-</form>
-</body>
-
-
+    <body style="background-color: #999999;">
+        <div id="header">
+            <img id="logo" src="images/Logo.jpg"onclick="window.location.href = 'index.php'">
+            Update event ID: <?php echo $row["id"];?>
+            <div id="btnLogout" onclick="window.location.href='logout_ppm.php';">Logout</div>
+        </div>
+        <div id="updateContent">
+            <form action = "update.php" method="post">
+                ID: <input type = "text" name="id" value=<?php echo $row["id"];?> readonly>
+                <p>Event Name: <input type="text" name="event_name" value=<?php echo $row["event_name"];?>>
+                <p>Details: <textarea name="details" value=<?php echo $row["details"];?>></textarea>
+                <p>Location: <input type="text" name="location_name" value=<?php echo $row["location_name"];?>>
+                <p>Officers: <input type="text" name="officers" value=<?php echo $row["officers"];?>>
+                <p>Starting Date: <input type="date" name="begin_date" value=<?php echo $row["begin_date"];?>>
+                <p>Ending Date: <input type="date" name="end_date" value=<?php echo $row ["end_date"];?>>
+                <p>Starting Time: <input type="text" name="start_time" value=<?php echo $row["start_time"];?>>
+                <p>Community Information: <textarea name="community_info" value=<?php echo $row["community_info"];?>></textarea>
+                <p>Community Contact: <textarea name="community_contact" value=<?php echo $row["community_contact"];?>></textarea>
+                <p>Comments: <textarea name="comments" value=<?php echo $row["comments"];?>></textarea>
+                <input type = "submit">
+            </form>
+        </div>
+    </body>
 </html>
