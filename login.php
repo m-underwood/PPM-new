d4f7c2505da682afb6937555846555c7bd68f6f5
m-underwood <56260924+m-underwood@users.noreply.github.com>
2020-05-03 22:09:52 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,35 @@
+<?php
+
+    session_start();
+    include "connectdatabase.php";
+
+    if(isset($_POST["ppm_username"])){
+        $ppm_username = $_POST["ppm_username"];
+    }
+    if(isset($_POST["ppm_password"])){
+        $ppm_password = $_POST["ppm_password"];
+    }
+
+    $sql = "SELECT * FROM users_tbl WHERE username = '$ppm_username' AND password = '$ppm_password'";
+    
+    $result = mysqli_query($con, $sql);
+
+    if(mysqli_num_rows($result) ==1)
+    {
+        echo "<script>alert('You have signed in!')</script>";
+        $_SESSION["ppm_username"] = $ppm_username;
+        echo"<script>window.open('index.php','_self')</script>"; 
+    
+exit();
+    }
+    else
+    {
+        echo "<script>alert('Username or password is incorrect!')</script>";
+		
+    }
+    
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
